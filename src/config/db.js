76b60473446309670dacf1428d7bf83236fb14e2
Sparshi Jain <j.sparshi@peerme.tech>
2026-02-19 00:50:48 +0530

    Added production DB config

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,25 @@
 const { Pool } = require('pg');
 
 const isProduction = process.env.NODE_ENV === 'production';
-
-const pool = new Pool({
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	ssl: isProduction ? { rejectUnauthorized: false } : false,
-	max: 10,
-	idleTimeoutMillis: 30000,
-	connectionTimeoutMillis: 2000
-});
+let pool;
+if (isProduction) {
+	pool = new Pool({
+		connectionString: process.env.DATABASE_URL,
+		ssl: { rejectUnauthorized: false }
+	});
+} else {
+	pool = new Pool({
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		ssl: false,
+		max: 10,
+		idleTimeoutMillis: 30000,
+		connectionTimeoutMillis: 2000
+	});
+}
 
 pool.on('connect', () => {
 	console.log('PostgreSQL connected');
